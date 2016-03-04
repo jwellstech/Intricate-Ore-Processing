d1dd344aa1a5eb79459322bb6b84180e90a605ed
Jordan <jwells.me@gmail.com>
2016-03-03 19:47:34 -0500

    hlp1

--- src/main/java/init/ModRecipes.java
+++ src/main/java/init/ModRecipes.java
@@ -24,11 +24,21 @@ public class ModRecipes {
 		GameRegistry.addRecipe(new ItemStack(Blocks.obsidian),"AAA","AAA","AAA",'A', Blocks.diamond_block);
 		//COPPER PROCESS
 //		GameRegistry.addShapelessRecipe(new ItemStack(ModItems.groundCopper), new ItemStack(ModItems.hammer, 1, OreDictionary.WILDCARD_VALUE), copperOre);
-		//paraffing oil not a block yet.... GameRegistry.addRecipe(new ItemStack(ModItems.chalcopyrite), ModItems.groundCopper, ModItems.parrafinOil);
-		GameRegistry.addShapelessRecipe(new ItemStack(ModItems.copperMatte), ModItems.calcine, ModItems.limestoneShard);
+//		GameRegistry.addRecipe(new ItemStack(ModItems.chalcopyrite), ModItems.groundCopper, ModItems.parrafinOil);
+//		GameRegistry.addShapelessRecipe(new ItemStack(ModItems.copperMatte), ModItems.calcine, ModItems.limestoneShard);
+		//Coal Process
+		GameRegistry.addShapelessRecipe(new ItemStack(Items.coal), new ItemStack(ModItems.hammer, 1, OreDictionary.WILDCARD_VALUE), poorCoalOre.StartupCommon.blockPoorCoalOre, poorCoalOre.StartupCommon.blockPoorCoalOre);
+		GameRegistry.addShapelessRecipe(new ItemStack(Items.coal), new ItemStack(ModItems.hammer, 1, OreDictionary.WILDCARD_VALUE), Blocks.coal_ore);
+		GameRegistry.addShapelessRecipe(new ItemStack(Items.coal, 2), new ItemStack(ModItems.hammer, 1, OreDictionary.WILDCARD_VALUE), richCoalOre.StartupCommon.blockRichCoalOre);
+		
 		//IRON PROCESS
 		GameRegistry.addShapelessRecipe(new ItemStack(ModItems.groundIron), new ItemStack(ModItems.hammer, 1, OreDictionary.WILDCARD_VALUE), Blocks.iron_ore);
-		//GOLD PROCESS
+		GameRegistry.addShapelessRecipe(new ItemStack(ModItems.ironLump), ModItems.groundIron, Items.coal);
+		GameRegistry.addShapelessRecipe(new ItemStack(Items.iron_ingot), ModItems.ironSlag, ModItems.ironSlag);
+		//GOLD PROCESS  need ground gold
+//		GameRegistry.addShapelessRecipe(new ItemStack(ModItems.groundGold), new ItemStack(ModItems.hammer, 1, OreDictionary.WILDCARD_VALUE), Blocks.gold_ore);
+//		GameRegistry.addShapelessRecipe(new ItemStack(ModItems.amalgam), ModItems.groundGold, Items.flint);
+		GameRegistry.addShapelessRecipe(new ItemStack(Items.gold_ingot), ModItems.amalgam, ModItems.cyanide);
 		//REDSTONE PROCESS
 		GameRegistry.addShapelessRecipe(new ItemStack(Items.redstone, 4 ), new ItemStack(ModItems.hammer, 1, OreDictionary.WILDCARD_VALUE), poorRedstoneOre.StartupCommon.blockPoorRedstoneOre, poorRedstoneOre.StartupCommon.blockPoorRedstoneOre);
 		GameRegistry.addShapelessRecipe(new ItemStack(Items.redstone, 4), new ItemStack(ModItems.hammer, 1, OreDictionary.WILDCARD_VALUE), Blocks.redstone_ore);
@@ -41,12 +51,20 @@ public class ModRecipes {
 		GameRegistry.addShapelessRecipe(new ItemStack(Items.diamond), new ItemStack(ModItems.sandpaper, 1, OreDictionary.WILDCARD_VALUE), ModItems.roughDiamond);
 		
 		//LAPIS LAZULI PROCESS
+		GameRegistry.addShapelessRecipe(new ItemStack(Items.dye, 6, 4), new ItemStack(ModItems.hammer, 1, OreDictionary.WILDCARD_VALUE), poorLapisOre.StartupCommon.blockPoorLapisOre, poorLapisOre.StartupCommon.blockPoorLapisOre);
+		GameRegistry.addShapelessRecipe(new ItemStack(Items.dye, 6, 4), new ItemStack(ModItems.hammer, 1, OreDictionary.WILDCARD_VALUE), Blocks.lapis_ore);
+		GameRegistry.addShapelessRecipe(new ItemStack(Items.dye, 12, 4), new ItemStack(ModItems.hammer, 1, OreDictionary.WILDCARD_VALUE), richLapisOre.StartupCommon.blockRichLapisOre);
 		//TIN PROCESSs
 		//ALUMINIUM PROCESS
 		
 		//crafting items
 		GameRegistry.addShapedRecipe(new ItemStack(ModItems.hammer), "AAA", "AAA", " B ", 'A', Blocks.cobblestone, 'B', Items.stick);
 		GameRegistry.addShapedRecipe(new ItemStack(ModItems.sandpaper), "A", "B", "A", 'A', Blocks.sand, 'B', Items.paper, 'A', Blocks.sand);
+		GameRegistry.addShapedRecipe(new ItemStack(ModItems.diamondCuttingTool), " A", "B ", 'A', Items.stick, 'B', Items.diamond);
+		//other recipes
+		GameRegistry.addShapelessRecipe(new ItemStack(ModItems.roughDiamond), ModItems.uncutDiamond, ModItems.uncutDiamond);
+		GameRegistry.addShapelessRecipe(new ItemStack(ModItems.appleSeed, 4), Items.apple);
+		GameRegistry.addShapelessRecipe(new ItemStack(ModItems.cyanide), ModItems.appleSeed, ModItems.appleSeed);
 	}
 	
 	public static void removeCraftingRecipes() {
@@ -83,5 +101,6 @@ public class ModRecipes {
 	}
 	public static void removeSmeltingRecipes() {
 	//	List<>
+	
 	}
 }
